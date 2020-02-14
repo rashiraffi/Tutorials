099aa1ac199cfc94006d1b31446eaf0e50818746
rashiraffi <rashi1281@gmail.com>
2020-02-14 20:26:40 +0530

    Python files

--- PYTHON/shdb.py
+++ PYTHON/shdb.py
@@ -3,7 +3,6 @@ con=mysql.connector.connect(host="localhost",user="rashi",password="")
 cur=con.cursor()
 try:
 	dbs=cur.execute("Show databases")
-	print(dbs)
 except:
 	con.rollback()
 for i in cur:
---
+++ PYTHON/tkgrid.py
@@ -0,0 +1,11 @@
+from tkinter import *
+top=Tk()
+name=Label(top,text="Name")
+name.grid(row=0,column=0)
+e1=Entry(top)
+e1.grid(row=0,column=1)
+password=Label(top,text="Password").grid(row=1,column=0)
+e2=Entry(top)
+e2.grid(row=1,column=1)
+submit=Button(top,text="Submit").grid(row=2,column=1)
+top.mainloop()
