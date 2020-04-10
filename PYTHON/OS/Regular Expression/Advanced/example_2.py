0646d9d878045f1f5f42fce2904aaa5124472b3f
rashiraffi <rashi1281@gmail.com>
2020-04-10 20:25:11 +0530

    Advance RE example program

---
+++ PYTHON/OS/Regular Expression/Advanced/example_2.py
@@ -0,0 +1,11 @@
+#ong_words function returns all words that are at least 7 characters.
+
+import re
+def long_words(text):
+  pattern = r"\w{7,}"
+  result = re.findall(pattern, text)
+  return result
+
+print(long_words("I like to drink coffee in the morning.")) # ['morning']
+print(long_words("I also have a taste for hot chocolate in the afternoon.")) # ['chocolate', 'afternoon']
+print(long_words("I never drink tea late at night.")) # []
