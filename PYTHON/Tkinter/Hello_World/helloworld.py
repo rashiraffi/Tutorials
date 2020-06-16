a4fd8b114d469549ae0672eccc8ba8986f9506f0
Rashi Raffi <Rashi1281@gmail.com>
2020-06-16 10:50:51 +0530

    Tkinter programs added

---
+++ PYTHON/Tkinter/Hello_World/helloworld.py
@@ -0,0 +1,5 @@
+# !/usr/bin/python3
+import tkinter
+top=tkinter.Tk()
+hw = tkinter.Label(top,text='Hello World..').grid(row=0,column=0)
+top.mainloop()
---
+++ PYTHON/Tkinter/Hello_World/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import tkinter
+top = tkinter.Tk()
+top.mainloop()
