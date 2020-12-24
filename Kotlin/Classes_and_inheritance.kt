ef7fa16f9ec65ac64610233b05c2a08e1892d6d1
Rashi Raffi <Rashi1281@gmail.com>
2020-12-24 23:26:46 +0530

    Summary added

--- Kotlin/Classes_and_inheritance.kt
+++ Kotlin/Classes_and_inheritance.kt
@@ -171,3 +171,18 @@ class RoundTower(
        return super.floorArea() * floors
    }
 }
+
+
+/* Summary
+In this codelab you learned how to:
+
+Create a class hierarchy, that is a tree of classes where children inherit functionality from parent classes. Properties and functions are inherited by subclasses.
+Create an abstract class where some functionality is left to be implemented by its subclasses. An abstract class can therefore not be instantiated.
+Create subclasses of an abstract class.
+Use override keyword to override properties and functions in subclasses.
+Use the super keyword to reference functions and properties in the parent class.
+Make a class open so that it can be subclassed.
+Make a property private, so it can only be used inside the class.
+Use the with construct to make multiple calls on the same object instance.
+Import functionality from the kotlin.math library
+*/
