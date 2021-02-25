08dd44e19a7885bdf515c847b2224fa8094d1560
Rashi Raffi <Rashi1281@gmail.com>
2021-02-25 11:29:19 +0530

    Structure Updated

---
+++ Java/BASIC/Variables.java
@@ -0,0 +1,17 @@
+public class Variables
+{
+	public static void main(String [] args)
+	{
+		int a=10;
+		float b = 10.555f;
+		char c='d';
+		boolean e=true;
+		String f="This is a string";
+		System.out.println("A is a Integer variable having the value "+ a );
+		System.out.println("B is a Float variable having the value "+ b );
+		System.out.println("c is a Character variable having the value "+ c );
+		System.out.println("E is a Boolean variable having the value "+ e );
+		System.out.println("F is a String varible having the value ="+ f );
+	}
+}
+	
