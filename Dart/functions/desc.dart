4890f7ea69ffd24b704b4ad050915c33ffc86743
Rashi Raffi <Rashi1281@gmail.com>
2021-05-19 21:41:27 +0530

    function with return added

--- Dart/functions/desc.dart
+++ Dart/functions/desc.dart
@@ -1,9 +1,16 @@
 void main() {
   describe("RR", 22, 1.8);
+  final person1 = details("RRR", 23, 1.9);
+  print(person1);
 }
 
 // Describe is a function with no return
 void describe(String name, int age, double height)
 {
   print("Name = $name, Age = $age, Height= $height");
+}
+
+String details(String name, int age, double height)
+{
+  return "Name = $name, Age = $age, Height= $height";
 }
