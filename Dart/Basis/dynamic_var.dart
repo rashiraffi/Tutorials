8e016ff3c2601b5a50071f0261b1cc1597ecec02
Rashi Raffi <Rashi1281@gmail.com>
2021-05-19 21:27:21 +0530

    Dynamic var added

---
+++ Dart/Basis/dynamic_var.dart
@@ -0,0 +1,10 @@
+//dynamic varibles are type casting
+
+void main()
+{
+  dynamic variable = "RR";
+  print(variable);
+  //changing the variable to int data type
+  variable = 458;
+  print(variable);
+}
