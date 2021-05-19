ba61c0e6ba18356d30a0e57fff53cb79dfa15ab7
Rashi Raffi <Rashi1281@gmail.com>
2021-05-19 21:32:31 +0530

    Simple dart function added

---
+++ Dart/functions/desc.dart
@@ -0,0 +1,9 @@
+void main() {
+  describe("RR", 22, 1.8);
+}
+
+// Describe is a function with no return
+void describe(String name, int age, double height)
+{
+  print("Name = $name, Age = $age, Height= $height");
+}
