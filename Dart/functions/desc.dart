91d5b3b5a488549a18a7c54f70881cf717a94895
Rashi Raffi <Rashi1281@gmail.com>
2021-05-27 21:34:48 +0530

    Named parameter added

--- Dart/functions/desc.dart
+++ Dart/functions/desc.dart
@@ -6,6 +6,7 @@ void main() {
   final person1 = details("RRR", 23, height);
   print(person1);
   describe1("RRR", 23);
+  describe2(name : "RRR", age: 23, height: height);
 }
 
 // Describe is a function with no return
@@ -18,12 +19,20 @@ void describe(String name, int age, double height)
 // Describe is a function with return
 String details(String name, int age, double height)
 {
-  return "Name = $name, Age = $age, Height= $height";
+  return "Name = $name, Age = $age";
 }
 
 
 // Describe is a function with optional argument
 void describe1(String name, int age, [double height = 0.0])
+{
+  // here height is optional
+  print("Name = $name, Age = $age, Height= $height");
+  //Output Name = RRR, Age = 23, Height= 0.0
+}
+
+//Named parameter
+void describe2({String name = "", int age = 0, double height = 0.0})
 {
   // here height is optional
   print("Name = $name, Age = $age, Height= $height");
