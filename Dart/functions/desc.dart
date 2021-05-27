571348281046d3192027536f81b68377374a5191
Rashi Raffi <Rashi1281@gmail.com>
2021-05-27 21:15:54 +0530

    Optional argument added to funtion.

--- Dart/functions/desc.dart
+++ Dart/functions/desc.dart
@@ -1,7 +1,11 @@
+import 'dart:ffi';
+
 void main() {
-  describe("RR", 22, 1.8);
-  final person1 = details("RRR", 23, 1.9);
+  var height = 1.8;
+  describe("RR", 22, height);
+  final person1 = details("RRR", 23, height);
   print(person1);
+  describe1("RRR", 23);
 }
 
 // Describe is a function with no return
@@ -10,7 +14,18 @@ void describe(String name, int age, double height)
   print("Name = $name, Age = $age, Height= $height");
 }
 
+
+// Describe is a function with return
 String details(String name, int age, double height)
 {
   return "Name = $name, Age = $age, Height= $height";
+}
+
+
+// Describe is a function with optional argument
+void describe1(String name, int age, [double height = 0.0])
+{
+  // here height is optional
+  print("Name = $name, Age = $age, Height= $height");
+  //Output Name = RRR, Age = 23, Height= 0.0
 }
