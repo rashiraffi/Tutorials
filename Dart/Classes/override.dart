a2edaa2d9cd57179e7603259d15133a6fb01a23c
Rashi Raffi <Rashi1281@gmail.com>
2021-05-28 21:26:42 +0530

    Dart function override code added.

---
+++ Dart/Classes/override.dart
@@ -0,0 +1,39 @@
+void main(){
+
+  //Immunatble because of final keyword
+  //final person = Person("RR",23,1.8,67);
+  //person.name = "RRR";
+  //Cannot change the variable because it's immutable.
+  //print(person.details());
+  //print("BMI = ${person.bmi()}");
+  final employee = Employee("RRR",22,1.9,65,"iDontKnow",65000);
+  //print("Employee: ${employee.name} \nSalary: ${employee.salary}");
+  print(employee.toString());
+
+}
+
+class Person{
+  final String name;
+  final int age;
+  final double weight;
+  final double height;
+
+  Person(this.name,this.age,this.height,this.weight);
+  String details()
+  {
+    return "Name = $name, Age = $age, Height = $height, Weight = $weight";
+  }
+  @override
+  String toString() => 'Name: $name, age: $age' ;
+  double bmi() => weight/(height*height);
+}
+
+class Employee extends Person{
+  // Employee(String name, int age, double height, double weight, this.taxcode, this.salary) : super(name,age,height,weight);
+  Employee(String name, int age, double height, double weight, this.taxcode, this.salary)
+   : super(name,age,height,weight);
+  final String taxcode;
+  final int salary;
+  @override
+  String toString() => "${super.toString()}, Salary: $salary";
+}
