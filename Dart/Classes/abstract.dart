884d8dc9f4d0e6ccb4937b14a4de4f38164469e0
Rashi Raffi <Rashi1281@gmail.com>
2021-05-29 21:45:19 +0530

    Abstract class added.

--- Dart/Classes/abstract.dart
+++ Dart/Classes/abstract.dart
@@ -2,8 +2,15 @@ void main(){
 
   final square = Square(10);
   print("Area of square: ${square.area()}");
+  printArea(square);
 
 }
+
+//Class is passed as an argument
+void printArea(Shape shape){
+  print(shape.area());
+}
+
 //Class is declared
 abstract class Shape{
   double area();
